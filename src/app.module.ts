59f37ecb0c0913741e47080109a1adbdf40924d8
vladimir levadny <vladimir.levadny@gmail.com>
2023-06-04 07:23:40 +0300

    Add start confihuration

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.model';
+
+const env = process.env;
+
+@Module({
+	controllers: [],
+	providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${env.NODE_ENV}.env`
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: env.POSTGRES_HOST,
+			port: Number(env.POSTGRAS_PORT),
+			username: env.POSTGRES_USER,
+			password: env.POSTGRES_PASSWORD,
+			database: env.POSTGRES_DB,
+			models: [User],
+			autoLoadModels: true,
+		}),
+		UsersModule,
+	]
+})
+export class AppModule {
+
+}
